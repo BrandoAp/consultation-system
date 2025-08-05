0a94e55dac06c57cba06fe169b9ade6641c5e35e
BrandoAp <avilabrando14@gmail.com>
2025-08-05 15:40:40 -0500

    feat: reportes funcionando

--- src/connection/connection.java
+++ src/connection/connection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class connection {
     private static final String URL = "jdbc:mysql://localhost:3306/1gs221ds";
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "Programming/07";
 
     public static Connection getConnection() {
         try {
